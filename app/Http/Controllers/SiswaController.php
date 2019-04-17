0802ca1f7cfa498ce1e97605710e1ec0195b462a
Melan Eka Putri <melanputrimelan@gmail.com>
2019-04-17 23:00:06 +0700

    19. Membuat Nilai dalam Chart

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -80,7 +80,19 @@ class SiswaController extends Controller
     {
         $siswa = \App\Siswa::find($id);
         $matapelajaran = \App\Mapel::all();
-        return view('siswa.profile',['siswa' => $siswa,'matapelajaran' => $matapelajaran]);
+
+        // Data Chart -> Menampilkan nama mapel dan nilai
+        $categories = [];
+        $data = [];
+
+        // Looping data mapel dari database
+        foreach($matapelajaran as $mp){
+            if($siswa->mapel()->wherePivot('mapel_id', $mp->id)->first()){
+                $categories[] = $mp->nama; 
+                $data[] = $siswa->mapel()->wherePivot('mapel_id', $mp->id)->first()->pivot->nilai;
+            }
+        }
+        return view('siswa.profile',['siswa' => $siswa,'matapelajaran' => $matapelajaran, 'categories' => $categories,'data' => $data]);
     }
 
     public function addnilai(Request $request,$idsiswa)
--- resources/views/siswa/profile.blade.php
+++ resources/views/siswa/profile.blade.php
@@ -91,6 +91,11 @@
                                 </table>
                             </div>
                         </div>
+                        <div class="panel">
+                                <div id="chartNilai">
+        
+                                </div>
+                        </div>
                     </div>
                     <!-- END RIGHT COLUMN -->
                 </div>
@@ -127,7 +132,6 @@
                     <span class="help-block">{{$errors->first('nilai')}}</span>
                 @endif
             </div>
-         
         </div>
         <div class="modal-footer">
           <button type="submit" class="btn btn-primary">Simpan</button>
@@ -137,4 +141,46 @@
     </div>
   </div>
 
+@endsection
+
+@section('footer')
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script>
+        Highcharts.chart('chartNilai', {
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: 'Laporan Nilai Siswa'
+            },
+            xAxis: {
+                categories: {!!json_encode($categories)!!},
+                crosshair: true
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Nilai Mata Pelajaran'
+                }
+            },
+            tooltip: {
+                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                footerFormat: '</table>',
+                shared: true,
+                useHTML: true
+            },
+            plotOptions: {
+                column: {
+                    pointPadding: 0.2,
+                    borderWidth: 0
+                }
+            },
+            series: [{
+                name: 'Nilai',
+                data: {!!json_encode($data)!!}
+
+            }]
+        });
+
+    </script>
 @endsection
